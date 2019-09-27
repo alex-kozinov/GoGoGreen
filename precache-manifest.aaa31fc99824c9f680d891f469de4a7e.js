b3796bbe85c54ca41a3cafade153c97f2924eb3e
Aleksey <kozinov.av@gmail.com>
2019-09-27 11:36:38 +0300

    Updates

---
+++ precache-manifest.aaa31fc99824c9f680d891f469de4a7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f15a95f879993e0a0ca27491b1e260",
+    "url": "/GoGreen/index.html"
+  },
+  {
+    "revision": "09d77bd37e1e2060c394",
+    "url": "/GoGreen/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "09d77bd37e1e2060c394",
+    "url": "/GoGreen/static/js/2.63330368.chunk.js"
+  },
+  {
+    "revision": "e1f4902769fcbbab0d6f",
+    "url": "/GoGreen/static/js/main.fb0733dc.chunk.js"
+  },
+  {
+    "revision": "013441d9043053a5d12c",
+    "url": "/GoGreen/static/js/runtime-main.84ab447f.js"
+  }
+]);
