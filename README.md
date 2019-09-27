6534746c901fad9f151e02a32d563c9360089d87
Aleksey <kozinov.av@gmail.com>
2019-09-27 11:29:24 +0300

    write starting point

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# GoGoGreen
-Our project at Vk.Hack
+<div align="center">
+  <a href="https://github.com/VKCOM">
+    <img width="100" height="100" src="https://avatars3.githubusercontent.com/u/1478241?s=200&v=4">
+  </a>
+  <br>
+  <br>
+
+  [![npm][npm]][npm-url]
+  [![deps][deps]][deps-url]
+
+</div>
+
+## How to install
+
+* Clone this repository
+
+* Run command `npm install`
+
+## How to start work with app
+
+*  `npm start` — this will start dev server with hot reload on `localhost:10888`.
+
+* `npm run build` — this will build production bundle, with tree-shaking, uglify and all this modern fancy stuff
+
+* `npm run deploy` – this will update page at "github.io" 
+
+[npm]: https://img.shields.io/npm/v/@vkontakte/create-vk-mini-app.svg
+[npm-url]: https://npmjs.com/package/@vkontakte/create-vk-mini-app
+
+[deps]: https://img.shields.io/david/vkcom/create-vk-mini-app.svg
+[deps-url]: https://david-dm.org/vkcom/create-vk-mini-app
---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { View, Panel, Root, Epic, Tabbar, TabbarItem, PanelHeader} from '@vkontakte/vkui'
+import '@vkontakte/vkui/dist/vkui.css';
+
+import Icon28Newsfeed from '@vkontakte/icons/dist/28/newsfeed';
+import Icon28Search from '@vkontakte/icons/dist/28/search';
+import Icon28Place from '@vkontakte/icons/dist/28/place';
+import Icon28ArticleOutline from '@vkontakte/icons/dist/28/article_outline';
+
+
+
+class App extends React.Component {
+	constructor (props) {
+		super(props);
+
+		this.state = {
+			activeStory: 'history'
+		};
+		this.onStoryChange = this.onStoryChange.bind(this);
+	}
+
+	onStoryChange (e) {
+		this.setState({ activeStory: e.currentTarget.dataset.story })
+	}
+
+	render () {
+
+		return (
+			<Epic activeStory={this.state.activeStory} tabbar={
+				<Tabbar>
+					<TabbarItem
+						onClick={this.onStoryChange}
+						selected={this.state.activeStory === 'history'}
+						data-story="history"
+						text="История"
+					><Icon28ArticleOutline /></TabbarItem>
+					<TabbarItem
+						onClick={this.onStoryChange}
+						selected={this.state.activeStory === 'helper'}
+						data-story="helper"
+						text="Помощник"
+					><Icon28Search /></TabbarItem>
+					<TabbarItem
+						onClick={this.onStoryChange}
+						selected={this.state.activeStory === 'guides'}
+						data-story="guides"
+						text="Гайды"
+					><Icon28Newsfeed /></TabbarItem>
+					<TabbarItem
+						onClick={this.onStoryChange}
+						selected={this.state.activeStory === 'map'}
+						data-story="map"
+						text="Карта"
+					><Icon28Place /></TabbarItem>
+				</Tabbar>
+			}>
+				<View id="history" activePanel="history">
+					<Panel id="history">
+						<PanelHeader>История</PanelHeader>
+					</Panel>
+				</View>
+				<View id="helper" activePanel="helper">
+					<Panel id="helper">
+						<PanelHeader>Помощник</PanelHeader>
+					</Panel>
+				</View>
+				<View id="guides" activePanel="guides">
+					<Panel id="guides">
+						<PanelHeader>Гайды</PanelHeader>
+					</Panel>
+				</View>
+				<View id="map" activePanel="map">
+					<Panel id="map">
+						<PanelHeader>Карта</PanelHeader>
+					</Panel>
+				</View>
+			</Epic>
+		)
+	}
+}
+
+export default App;
+
