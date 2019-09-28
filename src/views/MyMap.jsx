5c7b9275811f226408a9a1605a0ebcb266d388ee
nevstruev dmitriy <nevstruev.dimk@yandex-team.ru>
2019-09-28 16:43:25 +0300

    icon

--- src/views/MyMap.jsx
+++ src/views/MyMap.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import For from 'react';
 import PropTypes from 'prop-types';
 import {PanelHeaderBack, Input, Panel, PanelHeader, HeaderButton, Search, Group, Footer, List, Cell, View, FixedLayout} from '@vkontakte/vkui';
 import Progress from "./Progress";
-import { ListBoxItem, ListBox, YMaps, Map, Placemark } from 'react-yandex-maps';
+import {Clusterer, ListBoxItem, ListBox, YMaps, Map, Placemark } from 'react-yandex-maps';
 import '../styles/main.css';
+
+import pointsPlastic from "./data/pointPlastic.json"
+import pointsPapes from "./data/pointsPapes.json"
+import pointsGlases from "./data/pointsGlases.json"
+
+import { tsConstructSignatureDeclaration } from '@babel/types';
 
 export default class MyMap extends React.Component {
     constructor(props) {
@@ -13,10 +20,54 @@ export default class MyMap extends React.Component {
             id: props.id,
             isPlastic: false,
             isPaper: false,
-            isSop: false
+            isGlases: false
         }
         this.changeSearch = this.changeSearch.bind(this);
+        this.getPointDataForGlases = this.getPointDataForGlases.bind(this);
+        this.getPointDataForPaper = this.getPointDataForPaper.bind(this);
+        this.getPointDataForPlastic = this.getPointDataForPlastic.bind(this);
+
+        this.getPointOptionsForGlases = this.getPointOptionsForGlases.bind(this);
+        this.getPointOptionsForPaper = this.getPointOptionsForPaper.bind(this);
+        this.getPointOptionsForPlastic = this.getPointOptionsForPlastic.bind(this);
     }
+
+
+    getPointDataForPlastic() {
+        return {
+            balloonContentBody: "Дом пластика",
+            clusterCaption: "Дом пластика"
+        };
+    };
+    getPointDataForPaper() {
+        return {
+            balloonContentBody: "Дом бумаги",
+            clusterCaption: "Дом бумаги"
+        };
+    };
+    getPointDataForGlases() {
+        return {
+            iconColor: "red",
+            balloonContentBody: "Время и стекло",
+            clusterCaption: "Время истекло"
+        };
+    };
+
+    getPointOptionsForPlastic() {
+        return {
+            iconColor: "red"
+        };
+    };
+    getPointOptionsForPaper() {
+        return {
+            iconColor: "green"
+        };
+    };
+    getPointOptionsForGlases() {
+        return {
+            iconColor: "red"
+        };
+    };
 
     changeSearch(prop) {
         if (prop === "plastic") {
@@ -27,17 +78,40 @@ export default class MyMap extends React.Component {
             let notPaper = !this.state.isPaper;
             this.setState({isPaper: notPaper});
         }
-        if (prop === "sop") {
-            let notSop = !this.state.isSop;
-            this.setState({isSop: notSop});
+        if (prop === "glases") {
+            let notGlases = !this.state.isGlases;
+            this.setState({isGlases: notGlases});
         }
 
-        console.log(this.state);
+        this.changeMap();
+    }
+
+    printFromJson(j, properties, options) {
+        return (j.map((coordinates, idx) => (
+            <Placemark
+              key={idx}
+              geometry={ [coordinates[0], coordinates[1]] }
+              properties={properties}
+              options={options}
+            />
+          )
+        )
+        )
+    }
+
+    printPlacemark() {
+        return ( <div>
+            { this.state.isPlastic && this.printFromJson(pointsPlastic, this.getPointDataForPlastic, this.getPointOptionsForPlastic)}
+            { this.state.isPaper && this.printFromJson(pointsPapes, this.getPointDataForPaper, this.getPointOptionsForPaper)}
+            { this.state.isGlases && this.printFromJson(pointsGlases, this.getPointDataForGlases, this.getPointOptionsForGlases)}
+            </div>
+            );
     }
 
     changeMap() {
         
     }
+
     render() {
         return (
             <View id={this.state.id} activePanel="mymap">
@@ -54,8 +128,21 @@ export default class MyMap extends React.Component {
                                     <ListBox data = {{ content: "Что относим на свалку?" }}>
                                         <ListBoxItem onClick={() => this.changeSearch("plastic")} data = {{ content: "Пластик" }} />
                                         <ListBoxItem onClick={() => this.changeSearch("paper")} data = {{ content: "Бумагу" }} />
-                                        <ListBoxItem onClick={() => this.changeSearch("sop")} data = {{ content: "Соперников" }} />
+                                        <ListBoxItem onClick={() => this.changeSearch("glases")} data = {{ content: "Соперников" }} />
                                     </ListBox>
+
+                                    <Clusterer
+                                        options={{
+                                        preset: "islands#invertedVioletClusterIcons",
+                                        groupByCoordinates: false,
+                                        clusterDisableClickZoom: true,
+                                        clusterHideIconOnBalloonOpen: false,
+                                        geoObjectHideIconOnBalloonOpen: false
+                                        }}
+                                    >
+
+                                    </Clusterer>
+                                    {this.printPlacemark()}
                                 </Map>
                         </YMaps>
                     </FixedLayout>
