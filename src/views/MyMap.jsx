b392fde03dc8f184d06b77273c0d90bc5f8403c6
nevstruev dmitriy <nevstruev.dimk@yandex-team.ru>
2019-09-28 13:52:41 +0300

    change to class

--- src/views/MyMap.jsx
+++ src/views/MyMap.jsx
@@ -5,33 +5,62 @@ import Progress from "./Progress";
 import { ListBoxItem, ListBox, YMaps, Map, Placemark } from 'react-yandex-maps';
 import '../styles/main.css';
 
-const MyMap = props => (
-    <View id={props.id} activePanel="mymap">
-        <Panel id="mymap">
-            <PanelHeader>Карта </PanelHeader>
-            <FixedLayout vertical="top bottom">
-                <YMaps>
-                        <Map className="all_window"
-                            defaultState = {{ 
-                                center: [55.751574, 37.573856], 
-                            zoom: 9 
-                                }}
-                        >
-                            <ListBox data = {{ content: "Что относим на свалку?" }}>
-                                <ListBoxItem data = {{ content: "Пластик" }} />
-                                <ListBoxItem data = {{ content: "Бумагу" }} />
-                                <ListBoxItem data = {{ content: "Соперников" }} />
-                            </ListBox>
-                        </Map>
-                </YMaps>
-            </FixedLayout>
-        </Panel>
-    </View>
-);
+export default class MyMap extends React.Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            id: props.id,
+            isPlastic: false,
+            isPaper: false,
+            isSop: false
+        }
+        this.changeSearch = this.changeSearch.bind(this);
+    }
 
-MyMap.propTypes = {
-    id: PropTypes.string.isRequired,
+    changeSearch(prop) {
+        if (prop === "plastic") {
+            let notPlasitc = !this.state.isPlastic;
+            this.setState({isPlastic: notPlasitc});
+        }
+        if (prop === "paper") {
+            let notPaper = !this.state.isPaper;
+            this.setState({isPaper: notPaper});
+        }
+        if (prop === "sop") {
+            let notSop = !this.state.isSop;
+            this.setState({isSop: notSop});
+        }
+
+        console.log(this.state);
+    }
+
+    changeMap() {
+        
+    }
+    render() {
+        return (
+            <View id={this.state.id} activePanel="mymap">
+                <Panel id="mymap">
+                    <PanelHeader>Карта </PanelHeader>
+                    <FixedLayout vertical="top bottom">
+                        <YMaps>
+                                <Map className="all_window"
+                                    defaultState = {{ 
+                                        center: [55.751574, 37.573856], 
+                                    zoom: 9 
+                                        }}
+                                >
+                                    <ListBox data = {{ content: "Что относим на свалку?" }}>
+                                        <ListBoxItem onClick={() => this.changeSearch("plastic")} data = {{ content: "Пластик" }} />
+                                        <ListBoxItem onClick={() => this.changeSearch("paper")} data = {{ content: "Бумагу" }} />
+                                        <ListBoxItem onClick={() => this.changeSearch("sop")} data = {{ content: "Соперников" }} />
+                                    </ListBox>
+                                </Map>
+                        </YMaps>
+                    </FixedLayout>
+                </Panel>
+            </View>
+        );
+    }
 };
-
-
-export default MyMap;
