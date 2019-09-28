0440d91003cd8e277a2e57e5f29a25ee358a2b67
rvg77 <gorb.roman1999@gmail.com>
2019-09-28 10:25:22 +0300

    Added general guide card item

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Panel, Root, Epic, Tabbar, TabbarItem, PanelHeader} from '@vkontakte/vkui'
+import {View, Panel, Root, Epic, Tabbar, TabbarItem, PanelHeader} from '@vkontakte/vkui'
 import '@vkontakte/vkui/dist/vkui.css';
 
 import Icon28Newsfeed from '@vkontakte/icons/dist/28/newsfeed';
@@ -14,58 +14,58 @@ import Map from './views/Map';
 
 
 class App extends React.Component {
-	constructor (props) {
-		super(props);
+    constructor(props) {
+        super(props);
 
-		this.state = {
-			activeStory: 'progress'
-		};
-		this.onStoryChange = this.onStoryChange.bind(this);
-	}
+        this.state = {
+            activeStory: 'progress'
+        };
+        this.onStoryChange = this.onStoryChange.bind(this);
+    }
 
-	onStoryChange (e) {
-		this.setState({ activeStory: e.currentTarget.dataset.story })
-	}
+    onStoryChange(e) {
+        this.setState({activeStory: e.currentTarget.dataset.story})
+    }
 
-	render () {
+    render() {
 
-		return (
-			<Epic activeStory={this.state.activeStory} tabbar={
-				<Tabbar>
-					<TabbarItem
-						onClick={this.onStoryChange}
-						selected={this.state.activeStory === 'progress'}
-						data-story="progress"
-						text="Прогрес"
-					><Icon28ArticleOutline /></TabbarItem>
-					<TabbarItem
-						onClick={this.onStoryChange}
-						selected={this.state.activeStory === 'helper'}
-						data-story="helper"
-						text="Помощник"
-					><Icon28Search /></TabbarItem>
-					<TabbarItem
-						onClick={this.onStoryChange}
-						selected={this.state.activeStory === 'guides'}
-						data-story="guides"
-						text="Гайды"
-					><Icon28Newsfeed /></TabbarItem>
-					<TabbarItem
-						onClick={this.onStoryChange}
-						selected={this.state.activeStory === 'map'}
-						data-story="map"
-						text="Карта"
-					><Icon28Place /></TabbarItem>
-				</Tabbar>
-			}>
-				<Progress id="progress"/>
-				<Helper id="helper"/>
-				<Guides id="guides"/>
-				<Map id="map"/>
+        return (
+            <Epic activeStory={this.state.activeStory} tabbar={
+                <Tabbar>
+                    <TabbarItem
+                        onClick={this.onStoryChange}
+                        selected={this.state.activeStory === 'progress'}
+                        data-story="progress"
+                        text="Прогрес"
+                    ><Icon28ArticleOutline/></TabbarItem>
+                    <TabbarItem
+                        onClick={this.onStoryChange}
+                        selected={this.state.activeStory === 'helper'}
+                        data-story="helper"
+                        text="Помощник"
+                    ><Icon28Search/></TabbarItem>
+                    <TabbarItem
+                        onClick={this.onStoryChange}
+                        selected={this.state.activeStory === 'guides'}
+                        data-story="guides"
+                        text="Гайды"
+                    ><Icon28Newsfeed/></TabbarItem>
+                    <TabbarItem
+                        onClick={this.onStoryChange}
+                        selected={this.state.activeStory === 'map'}
+                        data-story="map"
+                        text="Карта"
+                    ><Icon28Place/></TabbarItem>
+                </Tabbar>
+            }>
+                <Progress id="progress"/>
+                <Helper id="helper"/>
+                <Guides id="guides"/>
+                <Map id="map"/>
 
-			</Epic>
-		)
-	}
+            </Epic>
+        )
+    }
 }
 
 export default App;
--- src/components/guideitem/index.jsx
+++ src/components/guideitem/index.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import './styles.css';
-import { Panel, Cell, List, PanelHeader, Group, Div } from '@vkontakte/vkui';
+import {Panel, Cell, List, PanelHeader, Group, Div} from '@vkontakte/vkui';
 import {pages} from '../../constants'
+import Avatar from "@vkontakte/vkui/dist/components/Avatar/Avatar";
+import Button from "@vkontakte/vkui/dist/components/Button/Button";
 
 export class GuideItem extends React.Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
     }
 
     render() {
         return (
-            <Group title={this.props.title} description={this.props.description}>
+            <Group title={this.props.title}>
+                <Cell multiline
+                      size='l'
+                      description={this.props.description}
+                      before={<Avatar type="image" src="https://pp.userapi.com/c845220/v845220642/7cacc/XzhH5b7FSKY.jpg"
+                                      size={80}/>}
+                      bottomContent={<Button>Подробнее</Button>}
+                >
 
+                </Cell>
             </Group>
         );
     }
