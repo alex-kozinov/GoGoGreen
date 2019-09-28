4c9c433b97794d6324d8a2abacc1cd14bb12a45c
rvg77 <gorb.roman1999@gmail.com>
2019-09-28 22:13:03 +0300

    added drop down menu in progress

--- src/components/questpannel/index.jsx
+++ src/components/questpannel/index.jsx
@@ -6,41 +6,92 @@ import {
     PanelHeader,
     Group,
     Div,
-    Gallery,
     InfoRow,
     Progress,
+    Button,
     FixedLayout,
-    FormLayout
+    FormLayout,
+    HeaderContext,
+    PanelHeaderContent,
+    HeaderButton,
+    IOS,
+    platform
 } from '@vkontakte/vkui';
-import Button from "@vkontakte/vkui/dist/components/Button/Button";
 import PropTypes from "prop-types";
+import Icon24Done from '@vkontakte/icons/dist/24/done';
+import Icon24Settings from '@vkontakte/icons/dist/24/settings';
+import Icon24Users from '@vkontakte/icons/dist/24/users';
+import Icon16Dropdown from '@vkontakte/icons/dist/16/dropdown';
 
-class QuestPannel extends React.Component{
+class QuestPanel extends React.Component {
     constructor(props) {
         super(props);
+
+        this.osname = platform();
 
         this.state = {
             activePanel: "progress",
             progress: 40,
+            contextOpened: true,
+            mode: "plastic"
         };
 
         this.changeProgress = this.changeProgress.bind(this);
+        this.toggleContext = this.toggleContext.bind(this);
+        this.select = this.select.bind(this);
+    }
+
+    toggleContext() {
+        this.setState({contextOpened: !this.state.contextOpened});
+    }
+
+    select(e) {
+        const mode = e.currentTarget.dataset.mode;
+
+        this.setState({mode: mode});
+
+        requestAnimationFrame(this.toggleContext);
     }
 
     changeProgress(val) {
-        let new_progress = this.state.progress + val;
-        new_progress = Math.min(100, new_progress);
-        new_progress = Math.max(0, new_progress);
-        this.setState({progress: new_progress})
+        let newProgress = this.state.progress + val;
+        newProgress = Math.min(100, newProgress);
+        newProgress = Math.max(0, newProgress);
+
+        this.setState({progress: newProgress});
     }
-    
+
     render() {
         return <Panel id={this.props.id}>
-            <PanelHeader>Прогрес</PanelHeader>
+            <PanelHeader>
+                <PanelHeaderContent aside={<Icon16Dropdown/>} onClick={this.toggleContext}>
+                    {this.state.mode === 'plastic' ?  'Пластик' : 'Стекло'}
+                </PanelHeaderContent>
+            </PanelHeader>
+            <HeaderContext opened={this.state.contextOpened} onClose={this.toggleContext}>
+                <List>
+                    <Cell
+                        before={<Icon24Users />}
+                        asideContent={this.state.mode === "plastic" ? <Icon24Done fill="var(--accent)"/> : null}
+                        onClick={this.select}
+                        data-mode="plastic"
+                    >
+                        Пластик
+                    </Cell>
+                    <Cell
+                        before={<Icon24Settings />}
+                        asideContent={this.state.mode === "glass" ? <Icon24Done fill="var(--accent)"/> : null}
+                        onClick={this.select}
+                        data-mode="glass"
+                    >
+                        Стекло
+                    </Cell>
+                </List>
+            </HeaderContext>
             <Group>
                 <Div>
                     <InfoRow title="Прогресс озеленения">
-                        <Progress value={this.state.progress} />
+                        <Progress value={this.state.progress}/>
                     </InfoRow>
                 </Div>
             </Group>
@@ -62,8 +113,8 @@ class QuestPannel extends React.Component{
     }
 }
 
-QuestPannel.propTypes = {
+QuestPanel.propTypes = {
     id: PropTypes.string.isRequired,
 };
 
-export default QuestPannel;
+export default QuestPanel;
